15cf6eca1621e3ca7b36df711d7fe0e8cd703060
Tyler Young <tzyoung@valdosta.edu>
2025-02-27 16:28:36 -0500

    Created Project Added GUI file and the training model :)

---
+++ Project/Training/model_cnn.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, 3, padding='same')
+        self.conv2 = nn.Conv2d(16, 32, 3, padding='same')
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding='same')
+        self.fc1 = nn.Linear(1024, 512)
+        self.dropout = nn.Dropout(p=0.2)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
