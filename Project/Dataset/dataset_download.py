7a7331440d1c48bb5ae6fb49a9f814bc08fc2985
Elliot <mcartier445@gmail.com>
2025-03-02 16:11:32 -0500

    soft reset on branch to fix large file issue with pushing

---
+++ Project/Dataset/dataset_download.py
@@ -0,0 +1,41 @@
+from torchvision import datasets, transforms
+from torchvision.transforms import ToTensor
+from torch.utils.data import random_split, DataLoader
+
+def download_dataset():
+    # Dowload full CIFAR-100 training dataset
+    full_dataset = datasets.CIFAR100(
+        root='./data',
+        train=True,
+        download=True,
+        transform=ToTensor()
+    )
+    return full_dataset
+
+    '''
+    test_data = datasets.CIFAR100(
+        root='./data',
+        traing=False,
+        Download=True,
+        transform=ToTensor()
+    )
+    '''
+
+def split_data(full_dataset):
+    # get sizes for training set and validation set
+    train_size = int(0.8 * len(full_dataset))
+    val_size = int(0.2 * len(full_dataset))
+
+    # split dataset betwee training and validation
+    train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
+
+    # create dataloaders for training and validation
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+
+    return train_loader, val_loader
+
+
+if __name__ == '__main__':
+    dataset = download_dataset()
+    train_dataloader, val_dataloader = split_data(dataset)
