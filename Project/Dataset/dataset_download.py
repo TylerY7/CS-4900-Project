9837ad4335866b84202f3a8f353bd3d9c88cdda9
Elliot <mcartier445@gmail.com>
2025-03-05 14:09:34 -0500

    fixed syntax errors/argument erros in script

--- Project/Dataset/dataset_download.py
+++ Project/Dataset/dataset_download.py
@@ -3,7 +3,7 @@ from torchvision.transforms import ToTensor
 from torch.utils.data import random_split, DataLoader
 # subset_sampler or subset_random_sampler incase random split doesnt work
 
-def download_dataset():
+def download_train_dataset():
     # Dowload full CIFAR-100 training dataset
     full_dataset = datasets.CIFAR100(
         root='./data',
@@ -13,14 +13,14 @@ def download_dataset():
     )
     return full_dataset
 
-    '''
+def download_test_dataset():
     test_data = datasets.CIFAR100(
         root='./data',
-        traing=False,
-        Download=True,
+        train=False,
+        download=True,
         transform=ToTensor()
     )
-    '''
+    return test_data
 
 
 def split_data(full_dataset):
@@ -32,10 +32,9 @@ def split_data(full_dataset):
     train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
 
     return train_dataset, val_dataset
-    # create dataloaders for training and validation
 
-def get_data_loaders(train_dataset, val_dataset, batch_size):
-    return DataLoader(train_dataset, batch_size=batch_size, shuffle=True), DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+def get_data_loader(train_dataset, batch_size):
+    return DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
 if __name__ == '__main__':
     dataset = download_dataset()
