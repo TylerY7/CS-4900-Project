18823d1d40a9d323c198f34cc6935a54b22b4d09
Elliot <mcartier445@gmail.com>
2025-04-28 04:47:28 -0400

    Gui now can load a model and make predictions, also dynamically changes output based on labels used for training

--- Project/gui.py
+++ Project/gui.py
@@ -11,7 +11,7 @@ import sys
 base_dir = os.path.dirname(os.path.abspath(__file__))
 dataset_path = os.path.join(base_dir, 'Dataset')
 net_path = os.path.join(base_dir, 'Training')
-model_path = os.path.join(base_dir, 'models', 'model_Net_1745781891.582031.pt')
+model_path = os.path.join(base_dir, 'models', 'model_Net_150e-128bs-0.01lr-100cls_1745798565.021512.pt')
 sys.path.append(dataset_path)
 sys.path.append(net_path)
 
