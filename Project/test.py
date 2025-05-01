a848ea6b52666a516d009e38fe2b0151b468a4ae
Tyler Young <tzyoung@valdosta.edu>
2025-05-01 13:41:49 -0400

    Fixed mapping issue :3

--- Project/test.py
+++ Project/test.py
@@ -18,21 +18,48 @@ sys.path.append(dataset_path)
 import dataset_download
 import argparse
 
+
+# CIFAR-100 class labels (index = class id)
+classes = [
+    "apple", "aquarium_fish", "baby", "bear", "beaver", "bed", "bee", "beetle", "bicycle", "bottle",
+    "bowl", "boy", "bridge", "bus", "butterfly", "camel", "can", "castle", "caterpillar", "cattle",
+    "chair", "chimpanzee", "clock", "cloud", "cockroach", "couch", "crab", "crocodile", "cup", "dinosaur",
+    "dolphin", "elephant", "flatfish", "forest", "fox", "girl", "hamster", "house", "kangaroo", "keyboard",
+    "lamp", "lawn_mower", "leopard", "lion", "lizard", "lobster", "man", "maple_tree", "motorcycle", "mountain",
+    "mouse", "mushroom", "oak_tree", "orange", "orchid", "otter", "palm_tree", "pear", "pickup_truck", "pine_tree",
+    "plain", "plate", "poppy", "porcupine", "possum", "rabbit", "raccoon", "ray", "road", "rocket",
+    "rose", "sea", "seal", "shark", "shrew", "skunk", "skyscraper", "snail", "snake", "spider",
+    "squirrel", "streetcar", "sunflower", "sweet_pepper", "table", "tank", "telephone", "television", "tiger", "tractor",
+    "train", "trout", "tulip", "turtle", "wardrobe", "whale", "willow_tree", "wolf", "woman", "worm"
+]
+
+# Superclass labels
+superclasses = [
+    "aquatic_mammals", "fish", "flowers", "food_containers", "fruit_and_vegetables",
+    "household_electrical_devices", "household_furniture", "insects", "large_carnivores", "large_man-made_outdoor_things",
+    "large_natural_outdoor_scenes", "large_omnivores_and_herbivores", "medium_mammals", "non-insect_invertebrates",
+    "people", "reptiles", "small_mammals", "trees", "vehicles_1", "vehicles_2"
+]
+
+# Coarse labels per class (index = fine class ID, value = coarse class ID)
+coarse_labels = [
+    4, 1, 14, 8, 0, 6, 7, 7, 18, 3,
+    3, 14, 9, 18, 7, 11, 3, 9, 7, 11,
+    6, 11, 5, 10, 7, 6, 13, 15, 3, 15,
+    0, 11, 1, 10, 12, 14, 16, 9, 11, 5,
+    5, 19, 8, 8, 15, 13, 14, 17, 18, 10,
+    16, 4, 17, 4, 2, 0, 17, 4, 18, 17,
+    10, 3, 2, 12, 12, 16, 12, 1, 9, 19,
+    2, 10, 0, 1, 16, 12, 9, 13, 15, 13,
+    16, 19, 2, 4, 6, 19, 5, 5, 8, 19,
+    18, 1, 2, 15, 6, 0, 17, 8, 14, 13
+]
+
+# Final mapping: class name → superclass name
 class_to_superclass = {
-        'apple': 'fruit', 'orange': 'fruit', 'banana': 'fruit', 'pineapple': 'fruit', 'grape': 'fruit',
-        'peach': 'fruit', 'strawberry': 'fruit', 'cherry': 'fruit', 'lemon': 'fruit', 'pear': 'fruit',
-        
-        'aquarium_fish': 'fish', 'flatfish': 'fish', 'ray': 'fish', 'shark': 'fish', 'trout': 'fish',
-        
-        'orchid': 'flower', 'poppy': 'flower', 'rose': 'flower', 'sunflower': 'flower', 'tulip': 'flower',
-        
-        'bee': 'insect', 'beetle': 'insect', 'butterfly': 'insect', 'cockroach': 'insect', 'dragonfly': 'insect',
-        
-        'bear': 'mammal', 'cat': 'mammal', 'cow': 'mammal', 'dog': 'mammal', 'horse': 'mammal', 'elephant': 'mammal',
-        'kangaroo': 'mammal', 'lion': 'mammal', 'tiger': 'mammal', 'wolf': 'mammal',
-        
-        'rocket': 'vehicle', 'airliner': 'vehicle', 'cab': 'vehicle', 'ambulance': 'vehicle', 'fire_engine': 'vehicle'
-    }
+    classes[i]: superclasses[coarse_labels[i]]
+    for i in range(len(classes))
+}
 
 
 # Function to get class names
