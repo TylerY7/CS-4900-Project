4e8d1d2b6219831db6298b4b80e82a3cc40f3ee0
km <kmock262@gmail.com>
2025-05-02 13:16:12 -0400

    ignore

---
+++ Project/code/tensorboard_graphing.py
@@ -0,0 +1,40 @@
+"""
+This script opens TensorBoard in a new tab on a web browser 
+in order to display logged data from training such as training loss for saved models. 
+It is required that the user has run the train.py file first and has 
+kept the resulting saved files in the runs folder in order to have any information displayed.
+To shut down once started, press CTRL+C.
+"""
+
+import webbrowser
+from subprocess import Popen, DEVNULL
+import time
+import os
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+log_dir = os.path.join(base_dir, '../runs')
+
+# make sure path can be found, otherwise exit
+if not os.path.exists(log_dir):
+    print(f'ERROR: Log director \'{log_dir}\' not found.')
+    exit(1)
+
+# opens TB process
+print('Starting TensorBoard...')
+# stdout and stderr redirected as to not fill terminal with text
+tb_process = Popen(['tensorboard', '--logdir', log_dir], stdout=DEVNULL, stderr=DEVNULL)
+
+# wait before opening web server so that it has time to start
+time.sleep(5)
+
+# opens web browser to default URL used by tensorboard
+url = 'http://localhost:6006'
+print(f'Opening TensorBoard at {url}')
+webbrowser.open(url)
+
+print(f'TensorBoard is running at {url}\nPress CTRL+C to stop.')
+try:
+    tb_process.wait()
+except KeyboardInterrupt:
+    print('\nShutting down TensorBoard...')
+    tb_process.terminate()
